7995058a6f27e436edbb332f94bfb586aa4639ac
rgao <xinghoshi@gmail.com>
2019-12-20 02:32:31 -0800

    completed design for home page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button, Row, Col } from 'react-bootstrap'
 import "./Nav.css"
 
 class Nav extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark navbar-expand-lg">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/Coursework"}>Coursework</Link>
-        <Link to={"/Resources"}>Resources</Link>
-        <Link to={"/Contacts"}>Contacts</Link>
+      <nav className="navbar navbar-dark navbar-expand-lg justify-content-center py-0">
+        <Col lg={2} className="text-center nav-btn py-2" id="home-btn">
+          <Link className="nav-link" to={"/"}>Home</Link>
+        </Col>
+        <Col lg={2} className="text-center nav-btn py-2">
+          <Link className="nav-link" to={"/Coursework"}>Coursework</Link>        
+        </Col>
+        <Col lg={2} className="text-center nav-btn py-2">
+          <Link className="nav-link" to={"/Resources"}>Resources</Link>        
+        </Col>
+        <Col lg={2} className="text-center nav-btn py-2">
+          <Link className="nav-link" to={"/Contacts"}>Contacts</Link>
+        </Col>
       </nav>
     );
   }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 // import { Link } from "react-router-dom";
 import { Button, Row, Col } from 'react-bootstrap'
 import './Home.css'
+import pylogo from '../../assets/images/python-logo.png'
 
 class Home extends Component {
 
     render() {
         return (
             <div className="main">
-                <Row>
-                    <Col size="3">
-                        <img src="../../assets/images/python_logo.jpg"></img>
+                <Row className="justify-content-center my-5">
+                    <Col></Col>
+                    <Col md={10} xl={8} className="justify-content-center align-self-center text-center">
+                    <img className="img-responsive" id='python-logo' src={pylogo} />
+                    <h1 className="ml-3">Introduction to Programming with Python</h1>
+                    </Col>
+                    <Col></Col>
+                </Row>
+
+                <Row className="intro justify-content-center mt-5">
+                    <Col xs={10} md={8} lg={7} xl={6} id="code-editor">
+                        <p className="mx-1 my-4 intro-text"># Python is an intuitive, high-level language that serves as an excellent gateway into the world of programming.</p>
                     </Col>
                 </Row>
-                <h3>Welcome to Introduction to Programming with Python!</h3>
-
-                <p>This course will introduce to students the basics of programming, including working with Unix commands and working within a development environment.</p>
             </div>
         )
     }
