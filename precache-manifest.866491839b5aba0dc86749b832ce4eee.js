e964870da130c22c3b4b2bcf91698bfc04231388
rgao <xinghoshi@gmail.com>
2019-12-20 19:40:47 -0800

    Updates

---
+++ precache-manifest.866491839b5aba0dc86749b832ce4eee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee5d1ce1d97071bfefe8dbc1b2a4a96",
+    "url": "/python_course/index.html"
+  },
+  {
+    "revision": "d9b8ca777b5f2cbdb043",
+    "url": "/python_course/static/css/main.001f433b.chunk.css"
+  },
+  {
+    "revision": "0de6e631b2a91369c124",
+    "url": "/python_course/static/js/2.f955359a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/python_course/static/js/2.f955359a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9b8ca777b5f2cbdb043",
+    "url": "/python_course/static/js/main.a023a0c1.chunk.js"
+  },
+  {
+    "revision": "30531ed692d2d92d758d",
+    "url": "/python_course/static/js/runtime-main.3ebd5036.js"
+  },
+  {
+    "revision": "91a90e09fc61de147e200caa04e63c1d",
+    "url": "/python_course/static/media/flux_regular.91a90e09.otf"
+  },
+  {
+    "revision": "9953c99be17e58a0006bd1ec688d20c7",
+    "url": "/python_course/static/media/python-logo.9953c99b.png"
+  }
+]);
